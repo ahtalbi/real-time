57f6c888826660b8273c76f1a086269e24b36dc8
iaboudou <ilyassaboudou07@gmail.com>
2026-01-21 08:43:15 +0100

    change in IsUserExist

--- backend/controllers/checksession.go
+++ backend/controllers/checksession.go
@@ -8,12 +8,18 @@ import (
 func (c *Controller) HasSession(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte(`{"error":"method not allowed"}`))
+		return
+	}
+
 	_, er := c.DB.CheckSessionExistance(r)
 	if er != nil {
 		w.WriteHeader(http.StatusUnauthorized)
 		w.Write([]byte(`{"error":"session not found"}`))
-		return
-	} 
+		return   
+	}
 
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]interface{}{
