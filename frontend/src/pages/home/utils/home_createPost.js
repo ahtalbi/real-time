f0348dcb46fb873ef50712debb620d83c95cbe4e
iaboudou <ilyassaboudou07@gmail.com>
2026-02-16 23:59:07 +0000

    done

--- frontend/src/pages/home/utils/home_createPost.js
+++ frontend/src/pages/home/utils/home_createPost.js
@@ -7,16 +7,24 @@ export function initCreatePost() {
 
 	GlobalEventsManager.submit.RegisterEvent("postCreate", async (form) => {
 		const content = form.elements.content.value.trim();
-		const catsCheched = form.querySelectorAll('input[name="categories"]:checked');
-		const categoryType = Array.from(catsCheched).map(cb => cb.value).join(",");
 		if (content.length < 1 || content.length > 600) {
 			showAlert("the length of the post should be between 1 and 600");
 			return;
 		}
+		
+		const catsCheched = form.querySelectorAll('input[name="categories"]:checked');
+		const categoryType = Array.from(catsCheched).map(cb => cb.value).join(",");
 
+		let picture = document.getElementById("createpostImage");
+		const file = picture.files[0];
+		
 		const formData = new FormData();
 		formData.append("Content", content);
 		formData.append("CategoryType", categoryType);
+		if (picture.files.length > 0) {
+    		formData.append("Image", file);
+		}
+
 
 		let data = null;
 		try {
